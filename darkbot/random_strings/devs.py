0dee6847417b10c61021b13628ba105ab4ff8be4
DARKBOTZ <97666393+DARKBOTZ-OS@users.noreply.github.com>
2022-02-11 14:17:56 +0530

    Create devs.py

---
+++ darkbot/random_strings/devs.py
@@ -0,0 +1,19 @@
+DEVLIST = [
+    "1830670820"
+]
+
+
+HARMFUL = [
+    "SESSION",
+    "HEROKU_API_KEY",
+    "base64",
+    "bash",
+    "get_me()",
+    "phone",
+    "os.system",
+    "sys.stdout",
+    "sys.stderr",
+    "subprocess",
+    "DARKBOT_SESSION",
+    "session.save()",
+]
